90e1c06776293b1a908929ab3cfbf92c4935942e
Nando-jr <ebonghooladapo@yahoo.com>
2022-06-28 22:02:27 -0700

    returns a pointer to  2D array of integers (fix)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,6 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i, j;
-	
 	if (width + height < 2 || width < 1 || height < 1)
 		return (NULL);
 
@@ -36,4 +35,3 @@ int **alloc_grid(int width, int height)
 	}
 	return (grid);
 }
-
