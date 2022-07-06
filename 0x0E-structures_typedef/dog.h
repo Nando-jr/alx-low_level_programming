dbce17de11f29081fc4d8fd7d911e0a85f2e2011
Nando-jr <ebonghooladapo@yahoo.com>
2022-07-05 19:27:59 -0700

    defines new struct type (hot fix)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,15 @@
 
 typedef struct dog dog_tag;
 
-/* struct dog - stores name, age & owner of dog */
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
 
 struct dog
 {
